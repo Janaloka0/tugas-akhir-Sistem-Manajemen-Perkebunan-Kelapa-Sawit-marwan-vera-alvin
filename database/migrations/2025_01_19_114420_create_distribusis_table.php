d5bfb3a6a25a289766133f1b5204be116a84301b
iltaverapaula10 <iltaverapaula10@gmail.com>
2025-01-19 11:54:23 +0000

    resolve

--- database/migrations/2025_01_19_114420_create_distribusis_table.php
+++ database/migrations/2025_01_19_114420_create_distribusis_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('distribusis', function (Blueprint $table) {
+        Schema::create('distribusi', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
